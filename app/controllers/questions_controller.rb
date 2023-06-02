f392c8fa5d6be350fe0daac91879765a6976d343
Anna Ibarra <annibrra@gmail.com>
2023-06-01 19:19:54 -0600

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def ask; end
+
+  private
+
+  def coach_answer(question)
+    if question == 'I am going to work'
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
